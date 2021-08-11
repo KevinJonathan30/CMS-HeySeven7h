31970678c79cabd6e178732d180f92697b3b8870
Kevin Jonathan <kevinjonathan3010@gmail.com>
2021-08-11 18:32:45 +0700

    Added CRUD for student private attendance per student

--- enterprise/student_private.php
+++ enterprise/student_private.php
@@ -5,7 +5,7 @@ require_once 'connect.php';
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $id = trim($_POST["idDeletion"]);
     if($id != "") {
-        $sql = "DELETE FROM heyseven7h_student WHERE id=$id";
+        $sql = "DELETE FROM heyseven7h_private_attendance WHERE id=$id";
         if ($conn->multi_query($sql) === TRUE) {
             header("location:privateattendance.php");
         }   
@@ -24,7 +24,7 @@ if (isset($_SESSION["loggedin"])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Private Attendance - Enterprise</title>
+    <title>Student Attendance - Enterprise</title>
     <link href="css/styles.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
         crossorigin="anonymous" />
@@ -37,10 +37,11 @@ if (isset($_SESSION["loggedin"])) {
     <div id="layoutSidenav_content">
         <main>
             <div class="container-fluid">
-                <h1 class="mt-4">Private Attendance</h1>
+                <h1 class="mt-4">Student Attendance</h1>
                 <ol class="breadcrumb mb-4">
                     <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
-                    <li class="breadcrumb-item active">Private Attendance</li>
+                    <li class="breadcrumb-item"><a href="privateattendance.php">Private Attendance</a></li>
+                    <li class="breadcrumb-item active">Student Attendance</li>
                 </ol>
                 <a style="float: right;"><button data-toggle='modal' data-target='#addModal' class="btn btn-dark">Add
                         New</button></a>
@@ -48,35 +49,57 @@ if (isset($_SESSION["loggedin"])) {
                 <div class="card mb-4">
                     <div class="card-header">
                         <i class="fas fa-table mr-1"></i>
-                        Student List
+                        Student Attendance List
                     </div>
+                    <?php 
+                        if($_SERVER["REQUEST_METHOD"] == "GET") {
+                            if(isset($_GET["id"])) {
+                                $id = $_GET["id"];
+                            }
+                        }  
+                        $sql = "SELECT * FROM heyseven7h_student WHERE id=$id";
+                        $result = $conn->query($sql);
+
+                        $pricePerHour = 0;
+
+                        if ($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                              echo "<div style='margin-left: 1rem; margin-top: 1rem;'><p>Name: " . $row["name"]. "</p><p>Address: " . $row["address"]. "</p><p>Phone Number: " .
+                              $row["phone"]."</p><p>Price/Hour: Rp ".$row["priceperhour"]."</p></div>";
+                              $pricePerHour = $row["priceperhour"];
+                            } 
+                          }
+                    ?>
                     <div class="card-body">
                         <div class="table-responsive">
                             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                 <thead>
                                     <tr>
-                                        <th>Name</th>
-                                        <th>Address</th>
-                                        <th>Phone Number</th>
-                                        <th>Price/Hour</th>
+                                        <th>No</th>
+                                        <th>Date</th>
+                                        <th>Start</th>
+                                        <th>Finish</th>
+                                        <th>Total (Hours)</th>
+                                        <th>Subject</th>
+                                        <th>Topic</th>
+                                        <th>Comment</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <?php 
-                                                $tutor_id = $_SESSION["id"];
-                                                if($_SERVER["REQUEST_METHOD"] == "GET") {
-                                                    if(isset($_GET["id"])) {
-                                                        $tutor_id = $_GET["id"];
-                                                    }
-                                                }  
-                                                $sql = "SELECT * FROM heyseven7h_student WHERE tutor_id=$tutor_id";
+                                    $total_hours = 0;
+                                                $sql = "SELECT id, ROW_NUMBER() OVER(
+                                                    ORDER BY id) AS row_num, date, start, finish, EXTRACT(HOUR FROM TIMEDIFF(finish, start)) + (EXTRACT(MINUTE FROM TIMEDIFF(finish, start)) + EXTRACT(SECOND FROM TIMEDIFF(finish, start))/60)/60 AS difference, subject, topic, comment, student_id FROM heyseven7h_private_attendance WHERE student_id=$id";
                                                 $result = $conn->query($sql);
                        
                                                 if ($result->num_rows > 0) {
                                                   while($row = $result->fetch_assoc()) {
-                                                    echo "<td>" . $row["name"]. "</td><td>" . $row["address"]. "</td><td>" .
-                                                    $row["phone"]."</td><td>".$row["priceperhour"]."</td><td><a href='student_private.php?id=".$row["id"]."' <button class='btn btn-info' title='Attendance List'><i class='fas fa-bars'></i></button></a>&nbsp;
+                                                    $total_hours += $row["difference"];
+                                                    echo "<td>" . $row["row_num"]. "</td><td>" . $row["date"]. "</td><td>" .
+                                                    $row["start"]."</td><td>".$row["finish"]."</td><td>".
+                                                    floatval($row["difference"])."</td><td>".
+                                                    $row["subject"]."</td><td>".$row["topic"]."</td><td>".$row["comment"]."<td>
                                                     <button class='btn btn-warning' title='Edit' onclick='editPage(".$row["id"].")' data-toggle='modal' data-target='#editModal'><i class='fas fa-edit'></i></button>&nbsp;<button class='btn btn-danger' title='Delete' onclick='deletePage(".$row["id"].")' data-toggle='modal' data-target='#deleteModal'><i class='fas fa-trash'></i></button></td></tr></a>";
                                                   } 
                                                 }
@@ -84,6 +107,12 @@ if (isset($_SESSION["loggedin"])) {
                                             ?>
                                 </tbody>
                             </table>
+                            <p style="font-weight: bold; text-align: center;">
+                                <?php 
+                                $total_hours *= $pricePerHour;
+                                echo "Total Amount: $total_hours" ;
+                            ?>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -97,24 +126,28 @@ if (isset($_SESSION["loggedin"])) {
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Add Student</h5>
+                    <h5 class="modal-title" id="staticBackdropLabel">Add Attendance</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    <label>Name</label>
-                    <input type="text" id="StudentNameAdd" class="form-control">
-                    <label>Address</label>
-                    <input type="text" id="StudentAddressAdd" class="form-control">
-                    <label>Phone</label>
-                    <input type="text" id="StudentPhoneAdd" class="form-control">
-                    <label>Price/hour</label>
-                    <input type="number" value=0 min=0 id="StudentPriceAdd" class="form-control">
+                    <label>Date</label>
+                    <input type="date" id="DateAdd" class="form-control">
+                    <label>Start</label>
+                    <input type="time" id="StartAdd" step="1" class="form-control">
+                    <label>Finish</label>
+                    <input type="time" id="FinishAdd" step="1" class="form-control">
+                    <label>Subject</label>
+                    <input type="text" id="SubjectAdd" value="" class="form-control">
+                    <label>Topic</label>
+                    <input type="text" id="TopicAdd" value="" class="form-control">
+                    <label>Comment</label>
+                    <input type="text" id="CommentAdd" value="" class="form-control">
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button type="button" onclick="addStudent()" class="btn btn-warning"
+                    <button type="button" onclick="addEntry(<?php echo $id; ?>)" class="btn btn-warning"
                         data-dismiss="modal">Add</button>
                 </div>
             </div>
@@ -125,21 +158,25 @@ if (isset($_SESSION["loggedin"])) {
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Edit Tutor</h5>
+                    <h5 class="modal-title" id="staticBackdropLabel">Edit Attendance</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    <input type="hidden" id="StudentIDEdit" class="form-control" disabled>
-                    <label>Name</label>
-                    <input type="text" id="StudentNameEdit" class="form-control">
-                    <label>Address</label>
-                    <input type="text" id="StudentAddressEdit" class="form-control">
-                    <label>Phone</label>
-                    <input type="text" id="StudentPhoneEdit" class="form-control">
-                    <label>Price/hour</label>
-                    <input type="number" value=0 min=0 id="StudentPriceEdit" class="form-control">
+                    <input type="hidden" id="IDEdit" class="form-control">
+                    <label>Date</label>
+                    <input type="date" id="DateEdit" class="form-control">
+                    <label>Start</label>
+                    <input type="time" id="StartEdit" step="1" class="form-control">
+                    <label>Finish</label>
+                    <input type="time" id="FinishEdit" step="1" class="form-control">
+                    <label>Subject</label>
+                    <input type="text" id="SubjectEdit" value="" class="form-control">
+                    <label>Topic</label>
+                    <input type="text" id="TopicEdit" value="" class="form-control">
+                    <label>Comment</label>
+                    <input type="text" id="CommentEdit" value="" class="form-control">
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
@@ -154,13 +191,13 @@ if (isset($_SESSION["loggedin"])) {
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="staticBackdropLabel">Are you sure you want to delete this Student?</h5>
+                    <h5 class="modal-title" id="staticBackdropLabel">Are you sure you want to delete this entry?</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    All the student data related to attendance will be deleted too.
+                    This entry will be deleted permanently if you do.
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
@@ -181,20 +218,25 @@ if (isset($_SESSION["loggedin"])) {
     <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
     <script src="assets/demo/datatables-demo.js"></script>
     <script>
-    function addStudent() {
-        var name = document.getElementById("StudentNameAdd").value;
-        var address = document.getElementById("StudentAddressAdd").value;
-        var phone = document.getElementById("StudentPhoneAdd").value;
-        var price = document.getElementById("StudentPriceAdd").value;
-        if (name != "" && address != "" && phone != "" && price != "") {
+    function addEntry(id) {
+        var date = document.getElementById("DateAdd").value;
+        var start = document.getElementById("StartAdd").value;
+        var finish = document.getElementById("FinishAdd").value;
+        var subject = document.getElementById("SubjectAdd").value;
+        var topic = document.getElementById("TopicAdd").value;
+        var comment = document.getElementById("CommentAdd").value;
+        if (date != "" && start != "" && finish != "") {
             $.ajax({
-                url: "API/addStudent.php",
+                url: "API/addStudentAttendance.php",
                 type: "POST",
                 data: {
-                    name: name,
-                    address: address,
-                    phone: phone,
-                    price: price
+                    date: date,
+                    start: start,
+                    finish: finish,
+                    subject: subject,
+                    topic: topic,
+                    comment: comment,
+                    id: id
                 },
                 success: function(data) {
                     location.reload();
@@ -208,18 +250,20 @@ if (isset($_SESSION["loggedin"])) {
 
     function editPage(id) {
         $.ajax({
-            url: "API/getStudentDetail.php",
+            url: "API/getAttendanceDetail.php",
             type: "POST",
             data: {
                 id: id
             },
             dataType: 'json',
             success: function(data) {
-                document.getElementById("StudentIDEdit").value = data[0].id;
-                document.getElementById("StudentNameEdit").value = data[0].name;
-                document.getElementById("StudentAddressEdit").value = data[0].address;
-                document.getElementById("StudentPhoneEdit").value = data[0].phone;
-                document.getElementById("StudentPriceEdit").value = data[0].priceperhour;
+                document.getElementById("IDEdit").value = data[0].id;
+                document.getElementById("DateEdit").value = data[0].date;
+                document.getElementById("StartEdit").value = data[0].start;
+                document.getElementById("FinishEdit").value = data[0].finish;
+                document.getElementById("SubjectEdit").value = data[0].subject;
+                document.getElementById("TopicEdit").value = data[0].topic;
+                document.getElementById("CommentEdit").value = data[0].comment;
             },
             error: function(data) {
                 console.log(data);
@@ -228,21 +272,25 @@ if (isset($_SESSION["loggedin"])) {
     }
 
     function submitEdit() {
-        var id = document.getElementById("StudentIDEdit").value;
-        var name = document.getElementById("StudentNameEdit").value;
-        var address = document.getElementById("StudentAddressEdit").value;
-        var phone = document.getElementById("StudentPhoneEdit").value;
-        var price = document.getElementById("StudentPriceEdit").value;
-        if (name != "" && address != "" && phone != "") {
+        var id = document.getElementById("IDEdit").value;
+        var date = document.getElementById("DateEdit").value;
+        var start = document.getElementById("StartEdit").value;
+        var finish = document.getElementById("FinishEdit").value;
+        var subject = document.getElementById("SubjectEdit").value;
+        var topic = document.getElementById("TopicEdit").value;
+        var comment = document.getElementById("CommentEdit").value;
+        if (date != "" && start != "" && finish != "") {
             $.ajax({
-                url: "API/submitEditStudent.php",
+                url: "API/submitEditAttendance.php",
                 type: "POST",
                 data: {
                     id: id,
-                    name: name,
-                    address: address,
-                    phone: phone,
-                    price: price
+                    date: date,
+                    start: start,
+                    finish: finish,
+                    subject: subject,
+                    topic: topic,
+                    comment: comment,
                 },
                 success: function(data) {
                     location.reload();
