ce0e06f9837419651e29f018045ba4a3b66f16c9
Kevin Jonathan <kevinjonathan3010@gmail.com>
2021-08-11 15:43:43 +0700

    From now on, only admin can access core categories (dashboard and manage tutor)

--- enterprise/components/navbar.php
+++ enterprise/components/navbar.php
@@ -29,14 +29,14 @@
         <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
             <div class="sb-sidenav-menu">
                 <div class="nav">
+                    <?php 
+                    if($_SESSION["role"] == 0) {
+                    ?>
                     <div class="sb-sidenav-menu-heading">Core</div>
                     <a class="nav-link" href="index.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                         Dashboard
                     </a>
-                    <?php 
-                    if($_SESSION["role"] == 0) {
-                    ?>
                     <a class="nav-link" href="managetutor.php">
                         <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
                         Manage Tutor
