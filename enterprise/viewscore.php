3eced9c5ccf040901e9ec915f676d30177b47407
Kevin Jonathan <kevinjonathan3010@gmail.com>
2021-11-28 15:37:02 +0700

    Added delete feature in viewscore.php

--- enterprise/viewscore.php
+++ enterprise/viewscore.php
@@ -2,6 +2,16 @@
 session_start();
 require_once 'connect.php';
 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $id = trim($_POST["idDeletion"]);
+    if($id != "") {
+        $sql = "DELETE FROM heyseven7h_score WHERE id=$id";
+        if ($conn->multi_query($sql) === TRUE) {
+            header("location:viewscore.php");
+        }   
+    }
+}
+
 if (isset($_SESSION["loggedin"])) {
   
 ?>
@@ -46,17 +56,19 @@ if (isset($_SESSION["loggedin"])) {
                                         <th>Score</th>
                                         <th>Date Submitted</th>
                                         <th>Tryout Name</th>
+                                        <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <?php 
-                                                $sql = "SELECT heyseven7h_score.name, score, dateSubmitted, heyseven7h_tryout.name as tryout_name FROM `heyseven7h_score` JOIN `heyseven7h_tryout` ON `heyseven7h_score`.tryout_id = `heyseven7h_tryout`.id";
+                                                $sql = "SELECT heyseven7h_score.name, heyseven7h_score.id, score, dateSubmitted, heyseven7h_tryout.name as tryout_name FROM `heyseven7h_score` JOIN `heyseven7h_tryout` ON `heyseven7h_score`.tryout_id = `heyseven7h_tryout`.id";
                                                 $result = $conn->query($sql);
                                                 $scoreCount = $result->num_rows;                         
                                                 if ($scoreCount > 0) {
                                                   // output data of each row
                                                   while($row = $result->fetch_assoc()) {
-                                                    echo "<tr><td>" . $row["name"]. "</td><td>" . $row["score"]. "</td><td>" . $row["dateSubmitted"]. "</td><td>". $row["tryout_name"]."</td></tr>";
+                                                    echo "<tr><td>" . $row["name"]. "</td><td>" . $row["score"]. "</td><td>" . $row["dateSubmitted"]. "</td><td>". $row["tryout_name"]."</td><td>
+                                                    <button class='btn btn-danger' title='Delete' onclick='deleteScore(".$row["id"].")' data-toggle='modal' data-target='#deleteModal'><i class='fas fa-trash'></i></button></td></tr>";
                                                   } 
                                                 }
                                                 $conn->close();
@@ -71,6 +83,29 @@ if (isset($_SESSION["loggedin"])) {
         <?php include('components/footer.php'); ?>
     </div>
     </div>
+    <div class="modal fade" id="deleteModal" data-backdrop="static" data-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Are you sure you want to delete this score?</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    This score will be deleted permanently from database.
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                        <input type="hidden" name="idDeletion" id="idTemp" value=0>
+                        <button type="submit" class="btn btn-danger">Delete!</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
@@ -78,6 +113,11 @@ if (isset($_SESSION["loggedin"])) {
     <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
     <script src="assets/demo/datatables-demo.js"></script>
+    <script>
+    function deleteScore(id) {
+        document.getElementById("idTemp").value = id;
+    }
+    </script>
 </body>
 
 </html>
